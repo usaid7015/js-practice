58334a589004ba0939104c923a5406525b21d220
usaid7015 <49516724+usaid7015@users.noreply.github.com>
2019-06-20 17:52:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// let speed = 90
+//  if (speed > 60) {
+// console.log("you are overspeed");
+// } else {
+// console.log("not");
+// }
+
+// var tem = 41
+// if (tem > 40 ) {
+//   console.log("IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave", )
+
+// }
+// let account = 41;
+// if (account <= 10000000) {
+// alert("Congratulations! You are a millionaire" );
+// } else {
+    // alert("I'm sorry, you are not a millionaire" );
+// }
+
+// let isElectric = "false"; 
+// if (isElectric =="true") {
+//     console.log("manufactured by Tesla" );
+// } else {
+//     console.log("not manufactured by Tesla" );
+// }
+  
+// let user = "wajhat";
+// let login = "saad";
+// if(login == "wajhat"){
+//     console.log("Welcome, usaid!");
+// } else{
+//     console.log("Please log in to continue");
+// }
+
+let currentScore = 15;
+let highScore = 20;
+let result = currentScore;
+let need = highScore - currentScore;
+if (currentScore > highScore){
+    console.log("Congratulations!! Your new high score is") + console.log(result);
+} else{
+    console.log("Try Again. You just need" ) + console.log(need);
+}
+let number = 156 - 70;
+let evenNumber = number % 2;
+if(evenNumber == 0){
+    console.log("This is EVEN")
+} else {
+    console.log("This is ODD")
+}
