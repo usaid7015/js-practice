d16867a237370b0fa48cf7f315eaec6b5ac95e5e
usaid7015 <49516724+usaid7015@users.noreply.github.com>
2019-06-22 16:14:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
-// let speed = 90
+let speed = 80
 //  if (speed > 60) {
 // console.log("you are overspeed");
 // } else {
 // console.log("not");
 // }
 
-// var tem = 41
+// let tem = 41
 // if (tem > 40 ) {
 //   console.log("IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave", )
 
 // }
-// let account = 41;
-// if (account <= 10000000) {
-// alert("Congratulations! You are a millionaire" );
-// } else {
-    // alert("I'm sorry, you are not a millionaire" );
+
+// let isLogin = "true";
+// if (isLogin){
+// console.log("welcome,you")
+// }else {
+//     console.log("Please log in to continue")
 // }
 
-// let isElectric = "false"; 
-// if (isElectric =="true") {
+
+// let account = 41;
+// if (account <= 10000000) {
+//     console.log("Congratulations! You are a millionaire" );
+// } else {
+//     console.log("I'm sorry, you are not a millionaire" );
+// }
+
+
+// let isElectric = "true"; 
+// if (isElectric =="false") {
 //     console.log("manufactured by Tesla" );
 // } else {
 //     console.log("not manufactured by Tesla" );
 // }
-  
-// let user = "wajhat";
-// let login = "saad";
-// if(login == "wajhat"){
-//     console.log("Welcome, usaid!");
+
+
+// let currentScore = 15;
+// let highScore = 20;
+// let result = currentScore;
+// let need = highScore - currentScore;
+// if (currentScore > highScore){
+//     console.log("Congratulations!! Your new high score is") + console.log("40");
 // } else{
-//     console.log("Please log in to continue");
+//     console.log("Try Again. You just need" ) + console.log("20");
 // }
 
-let currentScore = 15;
-let highScore = 20;
-let result = currentScore;
-let need = highScore - currentScore;
-if (currentScore > highScore){
-    console.log("Congratulations!! Your new high score is") + console.log(result);
-} else{
-    console.log("Try Again. You just need" ) + console.log(need);
-}
-let number = 156 - 70;
-let evenNumber = number % 2;
-if(evenNumber == 0){
-    console.log("This is EVEN")
-} else {
-    console.log("This is ODD")
-}
+
+// let number = 6;
+// let evenNumber = number % 2;
+// if(evenNumber == 0){
+//     console.log("This is EVEN")
+// } else {
+//     console.log("This is ODD")
+// }
+
+// let balance= -20;
+// if(balance < 1){
+// console.log("Moazziz Sarif, aap ka mojooda balance iss call k lye naa kaafi hai");
+
+// } else if(balance <= 10){
+//     console.log("Moazziz sarif, aap ka balance khatam honay wala hai");
+//     console.log(" Ring Ring!!");
+// } else{
+//     console.log("Ring Ring");
+// }
+
+// let math = 10;
+// let english = 8;
+// let computer = 20;
+//  if(math>=10 || english >=8 || computer>=20){
+//      console.log("Passed");
+//  } else{
+//     console.log("Failed");
+//  }
+
+//  const costOfApple = 10;
+//  const costOfBanana = 8;
+//  const costOfMango = 15;
+
+//  let noOfApples = 5;
+//  let noOfMangoes = 2;
+//  let noOfBananas = 3;
+//  const aliBalance = 100;
+//  let totalCost = 5 * 10 * 3 * 15 + 2 * 8;
+//  if (totalCost <=  aliBalance){
+//      console.log("shopping Success");
+
+//  } else {
+//      console.log("Shopping Failed")
+//  }
